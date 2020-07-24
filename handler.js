b8fe10238d1e2386e41c9c5cff205c8e02626662
RJ Hill <raymondjohnhill@gmail.com>
2020-07-23 22:52:55 -0400

    Build a create and a health path

--- handler.js
+++ handler.js
@@ -1,11 +1,15 @@
 'use strict';
 
-module.exports.hello = async event => {
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+module.exports.health = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Your lambda is running!',
+        version: '',
         input: event,
       },
       null,
@@ -15,4 +19,20 @@ module.exports.hello = async event => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.create = async event => {
+  stripe.customers.create({
+    email: 'customer@example.com',
+  })
+    .then(customer => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          customerID: customer.id,
+          input: event
+        })
+      }
+    })
+    .catch(error => console.error(error));
 };

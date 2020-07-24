9a109cb41c3dce7299139049596e6bfb01d6c694
RJ Hill <raymondjohnhill@gmail.com>
2020-07-23 23:53:37 -0400

    Fixing junk

--- handler.js
+++ handler.js
@@ -13,25 +13,24 @@ module.exports.health = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
 module.exports.create = async event => {
-  import Stripe from 'stripe';
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-  stripe.customers.create({
-    email: 'customer@example.com',
-  })
+  const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+  
+  return stripe.customers.create({ email: 'customer@example.com' })
     .then(customer => {
+      console.log('here');
       return {
         statusCode: 200,
-        body: JSON.stringify({
-          customerID: customer.id,
-          input: event
-        })
+        body: JSON.stringify(
+          {
+            customerID: customer.id,
+            input: event
+          },
+          null,
+          2
+        )
       }
     })
     .catch(error => console.error(error));
